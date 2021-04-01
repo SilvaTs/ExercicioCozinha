6f3641ada56b6b37f9d14e7e3ea5ad7b73fc690e
SilvaTs <danielsilvatsi@outlook.com>
2021-03-31 21:03:43 -0400

    feat: implementação da classe Model Cozinha,Ingrediente e Funcionario

---
+++ src/exerciciocozinha/ExercicioCozinha.java
@@ -0,0 +1,13 @@
+package exerciciocozinha;
+
+/**
+ *
+ * @author Daniel
+ */
+public class ExercicioCozinha {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ src/exerciciocozinha/Funcionario.java
@@ -0,0 +1,28 @@
+package exerciciocozinha;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Funcionario {
+
+    private String nome;
+    private String atividade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getAtividade() {
+        return atividade;
+    }
+
+    public void setAtividade(String atividade) {
+        this.atividade = atividade;
+    }
+
+}
